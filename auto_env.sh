e8c781b0db45bbe3707637fee303c06b39f9f2e2
chenbo <chenbo@chenbo-mac-vm.(none)>
2014-04-01 17:24:12 +0800

    1

--- auto_env.sh
+++ auto_env.sh
@@ -1,8 +1,3 @@
-#sudo apt-get update
-#sudo apt-get upgrade -y
-#sudo apt-get install vim -y
-#sudo apt-get install git -y
-
 tools_dir="env_tools"
 supertab_tool="supertab"
 vimrc_text="syntax on\n\
@@ -13,12 +8,23 @@ set shiftwidth=4\n\
 set softtabstop=4\n\
 set nu"
 
+#sudo apt-get update
+#sudo apt-get upgrade -y
+#sudo apt-get install vim -y
+#sudo apt-get install git -y
+#git config --global core.editor "vim"
+
 mkdir $tools_dir && cd $tools_dir
 
+### vim
 rm -rf ~/.vimrc
 echo -e $vimrc_text>~/.vimrc
 
 git clone https://github.com/ervandew/supertab.git
 cd $supertab_tool && make && make install
 
+### git auto completion
+wget https://github.com/git/git/blob/master/contrib/completion/git-completion.bash
+cp -rf git-completion.bash ~/.git-completion.sh
+echo "source ~/.git-completion.sh" >> ~/.bashrc
 
