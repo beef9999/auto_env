0feacc6d0fd2166af5582fdd9eaa1b9b3680e8ad
chenbo02 <chenbo02@chenbo02-vm.localdomain>
2014-04-18 17:48:19 +0800

    Add git shortcuts.

--- auto_env.sh
+++ auto_env.sh
@@ -1,30 +1,33 @@
+root_dir='pwd'
 tools_dir="env_tools"
 supertab_tool="supertab"
-vimrc_text="syntax on\n\
-syntax enable\n\
-filetype plugin indent on\n\
-set completeopt=longest,menu\n\
-set shiftwidth=4\n\
-set softtabstop=4\n\
+vimrc_text="syntax on
+syntax enable
+filetype plugin indent on
+set completeopt=longest,menu
+set shiftwidth=4
+set softtabstop=4
 set nu"
 
 #sudo apt-get update
 #sudo apt-get upgrade -y
 #sudo apt-get install vim -y
 #sudo apt-get install git -y
-#git config --global core.editor "vim"
 
 mkdir $tools_dir && cd $tools_dir
 
 ### vim
 rm -rf ~/.vimrc
-echo -e $vimrc_text>~/.vimrc
+echo -e "$vimrc_text" > ~/.vimrc
 
 git clone https://github.com/ervandew/supertab.git
-cd $supertab_tool && make && make install
+cd $supertab_tool && make && make install && cd root_dir
 
 ### git auto completion
 wget https://github.com/git/git/blob/master/contrib/completion/git-completion.bash
 cp -rf git-completion.bash ~/.git-completion.sh
 echo "source ~/.git-completion.sh" >> ~/.bashrc
 
+### git shortcuts
+cp -rf gitconfig ~/.gitconfig
+git config --global core.editor "vim"
