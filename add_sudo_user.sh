210e1d2ae20f20b60c03b2d4214c707877a6c4f6
Your Name <you@example.com>
2014-09-12 17:52:26 +0800

    s

--- add_sudo_user.sh
+++ add_sudo_user.sh
@@ -6,6 +6,15 @@ login_user=$1
 
 ### add specified user into sudo group
 
+function requires_root {
+    if [[ $EUID -ne 0 ]]; then
+        echo "ERROR: You need sudo to run the program" 2>&1
+        exit
+    fi
+}
+
+requires_root
+
 if [ ! -n "$login_user" ]; then
     echo "Specify user"
     exit
